81c9fef2d200bff9ddb603e8e866d94a9266c38d
Hossam Adel <hossamgeo@gmail.com>
2022-12-09 14:24:48 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,6 +22,7 @@ int main(void)
 			{
 				putchar((n % 10) + '0');
 				putchar((m % 10) + '0');
+				putchar((l % 10) + '0');
 				if (n == 7 && m == 8 && l == 9)
 					continue;
 				putchar(',');
