f545207f6e6963b37045aafccf199294491929a2
Hossam Adel <hossamgeo@gmail.com>
2022-12-09 03:35:04 -0800

    Task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Print weather number is negative or positive
+ * 	
+ * Return : Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,13 +9,12 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if ((n % 5) >= 1)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 5);
 	else if ((n % 6) < 0 && (n % 6) != 0)
