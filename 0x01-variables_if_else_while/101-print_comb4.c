99bcd097b4df27a8a2931b025c4d3b732c71d6d3
Hossam Adel <hossamgeo@gmail.com>
2022-12-09 14:21:50 -0800

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Print the combaination of three digits of all the numbers
+ * from 0 to 9 with "," and " "
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	int m;
+	int l;
+
+	for (n = 0; n <= 7; n++)
+	{
+		for (m = n + 1; m <= 8; m++)
+		{
+			for (l = n + 2; l <= 9; l++)
+			{
+				putchar((n % 10) + '0');
+				putchar((m % 10) + '0');
+				if (n == 7 && m == 8 && l == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
