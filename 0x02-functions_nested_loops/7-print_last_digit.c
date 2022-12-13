b6b0b658a030f0ffcd2e99b88bc28cbddcb4a5d3
Hossam Adel <hossamgeo@gmail.com>
2022-12-13 12:19:50 -0800

    Task 07

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * int print_last_digit - function to print last digit
+ * @n: number for test
+ * Return: 0 Always
+ */
+int print_last_digit(int n)
+{
+	_putchar((abs(n) % 10) + '0');
+	return (0);
+}
