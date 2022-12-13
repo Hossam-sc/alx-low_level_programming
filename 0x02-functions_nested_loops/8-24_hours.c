5db5f498c3a51ec38dd33341672e75e6534a9de0
Hossam Adel <hossamgeo@gmail.com>
2022-12-13 13:30:34 -0800

    Task 08

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * jack_bauer - function to print every minute of the day
+ * @N,M: numbers for test
+ * Return: 0 Always
+ */
+void jack_bauer(void)
+{
+	int N, M;
+
+	for (N = 0; N <= 23; N++)
+	{
+		for (M = 0; M <= 59; M++)
+		{
+			_putchar((N / 10) + '0');
+			_putchar((N % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			if (N == 23 && M == 59)
+				continue;
+			_putchar('\n');
+		}
+	}
+}
