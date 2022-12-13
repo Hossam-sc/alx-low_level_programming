8ca3de684bc5e7812e24eecfacb5d0e8d0a865d3
Hossam Adel <hossamgeo@gmail.com>
2022-12-13 10:37:42 -0800

    Task 03

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - Function that checks for lowercase character
- *
+ * @c: character to test
  * Return: 1 if lowercase ,else 0
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return (0);
-	else
 		return (1);
+	else
+		return (0);
 }
