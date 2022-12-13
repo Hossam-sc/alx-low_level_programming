e7a904e379d01eea534a7c7c89b732a9af8cea11
Hossam Adel <hossamgeo@gmail.com>
2022-12-13 11:11:49 -0800

    Task 05

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,31 +7,22 @@
  */
 int print_sign(int n)
 {
-	if (n > '0')
+	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(' ');
 		return (1);
 	}
 
-	else if (n < '0')
+	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar(' ');
 		return (-1);
 	}
 
-	else if (n == '0')
+	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(' ');
 		return (0);
 	}
-	else
-	{
-		_putchar('-');
-		_putchar(' ');
-		return ('/');
-	}
-	_putchar('\n');
+	
 }
