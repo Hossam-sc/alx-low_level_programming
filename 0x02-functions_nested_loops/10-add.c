eab2392bff299ac6c1a8923af11e626b2980d460
Hossam Adel <hossamgeo@gmail.com>
2022-12-13 15:38:34 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - function to print added of two integers
+ * @N: number for add
+ * @M: number 2 for add
+ * Return: 0 Always
+ */
+int add(int N, int M)
+{
+	return (N + M);
+}
