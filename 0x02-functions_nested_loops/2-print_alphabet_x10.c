b008d3736ca9997e89f778623c208a5944755ed9
Hossam Adel <hossamgeo@gmail.com>
2022-12-13 09:59:44 -0800

    Task 02

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that prints the alphabet 10 times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i <= 11; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+}
+
