9b57faad9fd28e239284202ff228a9873693efc0
Hossam Adel <hossamgeo@gmail.com>
2022-12-15 13:05:18 -0800

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square -
+ *@size: size of square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (j = 1; j <= size; j++)
+		{
+			for (i = 1; i <= size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
