7e710b732e4343b231ea5db6fe3662aeab0adcf2
Hossam Adel <hossamgeo@gmail.com>
2022-12-15 13:13:23 -0800

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -16,13 +16,14 @@ void print_square(int size)
 	}
 	else
 	{
-		for (j = 1; j <= size; j++)
+		for (i = 1; i <= size; i++)
 		{
-			for (i = 1; i <= size; i++)
+			for (j = 1; j <= size; j++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
 		}
 	}
+	_putchar('\n');
 }
