6a7c11e43e0b5c469025f6d0e20d48f8734be4e5
Hossam Adel <hossamgeo@gmail.com>
2022-12-15 11:26:15 -0800

    lets_go

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,17 @@ void more_numbers(void)
 	int i, j, Tens, Units;
 
 	for (i = 0; i <= 10; i++)
+	{
 		for (j = 0; j <= 14; j++)
+		{
 			Units = j % 10;
 			Tens = j / 10;
 			if (j > 9)
+			{
 				_putchar(Tens + '0');
+			}
 			_putchar(Units + '0');
+		}
 		_putchar('\n');
+	}
 }
