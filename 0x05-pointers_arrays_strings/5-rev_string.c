07c83843d9bb1d091805fec4ecac02a3de193e78
Hossam Adel <hossamgeo@gmail.com>
2022-12-19 07:29:20 -0800

    Task 05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - this characters in reverse.
+ *@s: this is a parameter to change.
+ *Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
