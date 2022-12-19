de1a85aa63531df0fdc6268cd1e67be7b4d61d72
Hossam Adel <hossamgeo@gmail.com>
2022-12-19 10:37:48 -0800

    Task 06

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,10 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != 0)
-	       	if ((i % 2) == 0)
+	while (i >= 0)
+		if ((i % 2) == 0)
 			_putchar(str[i]);
+		if (str[i] == '\0')
+			_putchar('\n');
 		i++;
-	_putchar('\n');
 }
