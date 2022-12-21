750fc24823f123fec557ddcf8757a422fe88d1b7
Hossam Adel <hossamgeo@gmail.com>
2022-12-21 08:14:08 -0800

    Task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _atoi - function that convert a string to an integer
+ *@s: pointer of string
+ * Return: 0 or numbers
+ */
+int _atoi(char *s)
+{
+	int i = 0, j = 1, sum = 0;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] < '0' || s[i] > '9') && (s[i] == '-'))
+			j = j * -1;
+i		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			sum = sum * 10 + (s[i] - '0');
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+		}
+		i++;
+	}
+	return (sum * j);
+}
