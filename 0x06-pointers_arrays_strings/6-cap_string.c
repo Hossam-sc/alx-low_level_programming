16ab10b65ac869c8430202cf7875a797c0e8324f
Hossam Adel <hossamgeo@gmail.com>
2022-12-22 04:38:41 -0800

    Task 06

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *cap_string - function that capitalizes all words of string
+ *@c: pointer of characters
+ * Return: c
+ */
+char *cap_string(char *c)
+{
+	int i, j;
+	int symb[14] = {' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}'};
+
+	for (i = 0; c[i] != 0; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'b')
+			c[i] = c[i] - 32;
+	}
+	for (j = 0; j < 14; j++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z' && c[i - 1] == symb[j])
+			c[i] = c[i] - 32;
+	}
+	return (c);
+}
