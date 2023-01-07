8bb5e7daed112de368109b33187b5b853974d72d
Hossam Adel <hossamgeo@gmail.com>
2023-01-07 06:08:19 -0800

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,18 +11,18 @@ int main(int argc, char *argv[])
 {
 	int i, mul = 1;
 
-	if (argc > 1 && argc < 3)
+	if (argc != 3)
 	{
-		for (i = 1, i < argc, i++)
-		{
-			mul = mul * atoi(argv[i]);
-		}
-		printf("%d\n", mul);
+		printf("%s\n", "Error");
+		return (0);
 	}
 	else
 	{
-		printf("%s\n", "Error");
-		return (1);
+		for (i = 1, i < argc, i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
 	}
 	return (0);
 }
