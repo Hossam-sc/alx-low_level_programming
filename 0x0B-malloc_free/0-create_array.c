00de500f77f5846d1dc993a09826a8386dd2cc33
Hossam Adel <hossamgeo@gmail.com>
2023-01-09 07:06:53 -0800

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - point entry
+ *@size: size input
+ *@c: charachters
+ * Return: 0 Always
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	array = malloc(sizeof(char) * size);
+
+	if (str == Null || size == 0)
+	{
+		return (Null);
+	}
+	for (i = 0; i < size; i++)
+	{
+		array[0] = c;
+	}
+	return (array);
+}
