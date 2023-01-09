5375c50420b36ed25da766c8b9a0448758251fe4
Hossam Adel <hossamgeo@gmail.com>
2023-01-09 11:33:27 -0800

    Task 02

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,28 +11,28 @@
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int size1 = 0, size2 = 0;
-	char *A1, *A2;
+	char *ptr, *ret;
 
-	A1 = s1;
+	ptr = s1;
 	if (s1)
-		while (*A1++)
+		while (*ptr++)
 			size1++;
 	else
 		s1 = "";
-	A2 = s2;
+	ptr = s2;
 	if (s2)
-		while (*A1++)
+		while (*ptr++)
 			size2++;
 	else
 		s2 = "";
-	A2 = malloc(size1 + size2 + 1);
-	if (!A2)
+	ret = malloc(size1 + size2 + 1);
+	if (!ret)
 		return (NULL);
-	A1 = A2;
+	ptr = ret;
 	while (*s1)
-		*A1++ = *s1++;
+		*ptr++ = *s1++;
 	while (*s2)
-		*A1++ = *s2++;
-	*A1 = 0;
-	return (A2);
+		*ptr++ = *s2++;
+	*ptr = 0;
+	return (ret);
 }
