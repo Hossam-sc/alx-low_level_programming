8d5145853cce7ad15185a9e1b2c1d88ec6157864
Hossam Adel <hossamgeo@gmail.com>
2023-01-19 03:33:10 -0800

    Task 01

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum all the parameters
+ *@n: varoadic inputs
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(valist, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(valist, const unsigned int);
+	}
+	va_end(valist);
+	return (sum);
+}
